542899fe73e5f97312d6a0a99c099de415308b0b
KaiHelsen <kai@helsen.info>
2022-05-30 11:03:02 +0200

    fixed namespace prefix issue

--- Includes/Hookables/SKEL_Abstract_Branch.php
+++ Includes/Hookables/SKEL_Abstract_Branch.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace SKEL\Includes\Hookables;
 
-use PWP\includes\hookables\PWP_I_Hookable_Component;
+use SKEL\includes\hookables\SKEL_I_Hookable_Component;
 
-abstract class SKEL_Abstract_Branch implements PWP_I_Hookable_Component
+abstract class SKEL_Abstract_Branch implements SKEL_I_Hookable_Component
 {
     use SKEL_Hookable_Branch_Trait;
 }
